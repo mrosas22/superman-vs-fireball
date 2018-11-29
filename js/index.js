9d203f5c745cfbe025941ad1529b790738a4c168
mrosas22 <miller.rosas22@gmail.com>
2018-11-29 15:11:55 -0500

    Added moving background

--- js/index.js
+++ js/index.js
@@ -1,18 +1,43 @@
+// get the canvas tag using .getElementById() method
 const myCanvas = document.getElementById('my-canvas');
 
 //conext has all the methods for drawing things
 const ctx = myCanvas.getContext('2d');
 
-
-//global variable
+//global variables
 let score = 0;
 let isOver = false;
-
+const img = new Image();
+    img.src = './images/background.png'
+    // ctx.fillStyle = '#00FFFF';
+const backgroundImg = {
+    img: img,
+    x: 0,
+    speed: -1,
+    move: function(){
+        this.x += this.speed;
+        this.x %= myCanvas.width; 
+    },
+    draw: function(){
+        ctx.drawImage(this.img,this.x,0, 1200, 500)
+        // ctx.drawImage(this.img, this.x, 0);
+        if (this.speed < 0){
+            ctx.drawImage(this.img, this.x + myCanvas.width, 0,1200,500);
+        }else{
+            ctx.drawImage(this.img, this.x - this.img.width,0,1200, 500)
+        };
+    }
+}
 
 function drawBackground(){
-    ctx.fillStyle = 'pink';
-    ctx.fillRect(0,0, 1000, 500);
-
+    // ctx.fillStyle = '#00FFFF';
+    
+    // 1000 ===> width of the canvas which I get from the index.html
+    // 500 ===> is the height of the canvas which I also get from index.html 
+    backgroundImg.move();
+    // ctx.fillRect(0,0, 1000, 500);
+    ctx.clearRect(0, 0, 100, 500);
+    backgroundImg.draw();
     //add some text
     ctx.fillStyle = 'Green';
     ctx.font = '30px Arial';
@@ -23,6 +48,7 @@ function drawBackground(){
 const fireballImg = new Image();
 const supermanImg = new Image();
 
+// "src" has to point as the image is used in HTML file
 fireballImg.src = './images/fireball.png';
 supermanImg.src = './images/superman.png';
 
@@ -31,17 +57,6 @@ let fireballY = 200;
 
 let supermanX = 0;
 let supermanY = 200;
-
-//Use for static objects in canvas
-// fireballImg.onload = function(){
-//     //
-//     ctx.drawImage(fireballImg,fireballX, fireballY, 80, 80);
-// }
-
-// supermanImg.onload = function(){
-//     //
-//     ctx.drawImage(supermanImg, supermanX, supermanY, 150, 150);
-// }
 
 //move superman
 document.onkeydown = function(event){
@@ -62,23 +77,25 @@ document.onkeydown = function(event){
     }
 }
 
-
 //animate the canvas
  
 function drawingLoop(){
     //erase the whole canvas before drawing again
-    ctx.clearRect(0, 0, 1000, 500);
+    ctx.clearRect(0, 0, 1200, 500);
 
     drawBackground();
+    // start moving fireball by changing it X coordinate in every loop call
     fireballX -= 5;
-
+    // when the fireball disappears from the canvas
     if(fireballX < -50){
-        fireballX = 1000;
-        fireballY = Math.floor(Math.random() * 450);
+        // set its x again to fireballX=1000
+        fireballX = 1200;
+        // and for each ball pick random Y in range 0 to 450 (which is height of the canvas - the height of the fireball)
+        fireballY = Math.floor(Math.random() * 400);
     }
 
     drawEverything()
-
+    // as long as isOver stays false, keep redrawing; 
     if(isOver === false){
     //re-draw the whole scene
     requestAnimationFrame(function(){
@@ -88,8 +105,8 @@ function drawingLoop(){
 }
 }
 
-
 function drawEverything(){
+    
     ctx.drawImage(fireballImg,fireballX, fireballY, 50, 50);
 
     ctx.drawImage(supermanImg, supermanX, supermanY, 150, 150);
@@ -99,36 +116,43 @@ function drawEverything(){
         gameOver();
     }
 
-
+    // manage the score here
     if(fireballX === 0){
         score++;
     }
 }
-
+// all 4 conditions need to be true in order to return true 
 function checkCollision (obj1x, obj1y, obj2x, obj2y){
-    //supermanY + superman-height >= fireballY
+    // supermanY + superman-height >= fireballY
     return obj1y + 150 - 50 >= obj2y
-        //supermanY <= fireballY
+        // supermanY <= fireballY + fireball-height
         && obj1y <= obj2y + 50
-        //supermanX + superman-width
+        // supermanX + superman-width >= fireballX
         && obj1x + 150 - 50 >= obj2x
+        // supermanX <= fireballX + fireball-width
         && obj1x <= obj2x + 50
 }
 
 function gameOver(){
-    ctx.clearRect(0,0, 1000, 500)
+    // clear the canvas because I don't want to see alive superman and the fireball
+    ctx.clearRect(0,0, 1200, 500)
+    // redraw the background
     drawBackground();
+    // create defeated_superman image
     const defeatedSuperman = new Image();
-    defeatedSuperman.src = "./images/defeated-superman.jpeg"
+    // point to the src where is the image itself
+    defeatedSuperman.src = "./images/defeated-superman.png"
+    //load defeteated superman img with coordinates x,y
     defeatedSuperman.onload = function (){
-        ctx.drawImage(defeatedSuperman, 480, 300, 150, 150)
+        ctx.drawImage(defeatedSuperman, 480, 340, 150, 150)
     }
+    //// change the value of isOver to true to finish the game
     isOver = true;
+    //display Game Over
     ctx.font = 'bold 70px Arial';
     ctx.fillStyle = 'red';
     ctx.fillText("Game Over", 400, 225);
 }
 
-
-
+// call drawingLoop() to start looping (after this point it will recursively call itself)
 drawingLoop();
